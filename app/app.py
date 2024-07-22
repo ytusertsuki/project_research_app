23f82e715dcb02f1748c42d78de8f22d6805faa7
dai-par <graphene0404@gmail.com>
2024-07-22 16:16:27 +0900

    エラー処理
    項目が埋まってない状態でPOSTするとエラーが出るようにした

--- app/app.py
+++ app/app.py
@@ -26,33 +26,41 @@ def plot():
         ku = request.form.get("retu_s")
         uu = request.form.get("retu_e")
 
-        s = int(su)
-        k = int(ku)
-        u = int(uu)
+        if d and su and ku and uu:
+            s = int(su)
+            k = int(ku)
+            u = int(uu)
 
 
-        data_set = np.loadtxt(
-            d,
-            dtype = "float",
-            delimiter = ",",
-            skiprows=s-1,
-            usecols=[k-1,u-1]
-        )
+            data_set = np.loadtxt(
+                d,
+                dtype = "float",
+                delimiter = ",",
+                skiprows=s-1,
+                usecols=[k-1,u-1]
+            )
 
-        for i in data_set:
-            plt.scatter(i[0],i[1])
-        #plt.plot(data_set[:, 0],data_set[:, 1])
-        io = BytesIO()
-        plt.savefig(io, format='png')
-        io.seek(0)
-        plt.close()
+            for i in data_set:
+                plt.scatter(i[0],i[1])
+            #plt.plot(data_set[:, 0],data_set[:, 1])
+            io = BytesIO()
+            plt.savefig(io, format='png')
+            io.seek(0)
+            plt.close()
 
-        data = base64.b64encode(io.getbuffer()).decode("ascii")
+            data = base64.b64encode(io.getbuffer()).decode("ascii")
 
-        return render_template(
-            'plot_index.html',
-            data = data
-        )
+            return render_template(
+                'plot_index.html',
+                data = data
+            )
+        
+        else:
+            erro = ("※入力項目に空きがあります")
+            return render_template(
+                'plot_index.html',
+                erro=erro
+            )
 
 
 #平均計算画面
